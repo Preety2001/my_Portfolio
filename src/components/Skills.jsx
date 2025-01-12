99e59c53f30fdbc1ddb008ea34a07cffbb14462d
Preety2001 <preety.bscsses21@iba-suk.edu.pk>
2025-01-12 17:05:38 +0500

    animated portfolio

---
+++ src/components/Skills.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+import skills from "./data/skills.json";
+
+const Skills = () => {
+  return (
+    <>
+      <div className="container skills" id="skills">
+        <h1>SKILLS</h1>
+        <div className="items">
+          {skills.map((data) => (
+            <>
+              <div
+                className="item"
+                key={data.id}
+                data-aos="fade-up"
+                data-aos-anchor-placement="top-bottom">
+              >
+                <img src={`/asstes/${data.imageSrc}`} alt="" />
+                <h3>{data.title}</h3>
+              </div>
+            </>
+          ))}
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Skills;
